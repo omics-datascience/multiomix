822da09e46ee8a436c3ffe87fb74637e5fa82b4b
Genarito <genarocamele@hotmail.com>
2021-07-13 19:48:31 -0300

    + Changed version to 4.6.2

--- src/multiomics_intermediate/settings.py
+++ src/multiomics_intermediate/settings.py
@@ -173,7 +173,7 @@ SECURE_REFERRER_POLICY = 'same-origin'
 # +++++ Custom settings +++++
 
 # Current Multiomix version
-VERSION: str = '4.6.1'
+VERSION: str = '4.6.2'
 
 # Max result DataFrame row count. If the number of rows is higher
 # the DataFrame is truncated. None for prevent truncation
