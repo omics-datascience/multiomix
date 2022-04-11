0712c6473ee26ae35e9a49b5b18af64bbbc30e68
Genaro Camele <genarocamele@hotmail.com>
2022-04-11 18:17:07 -0300

    Added filters and fields in Institutions admin panel

--- src/institutions/admin.py
+++ src/institutions/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Institution, InstitutionAdministration
 
-admin.site.register(Institution)
+
+class InstitutionAdmin(admin.ModelAdmin):
+    fields_display_search = ('name', 'location', 'email', 'telephone_number')
+    list_display = fields_display_search
+    search_fields = fields_display_search
+
+
+admin.site.register(Institution, InstitutionAdmin)
 admin.site.register(InstitutionAdministration)
