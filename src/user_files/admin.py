f4e5ca778b5ef6cf2577ec4d1f4bd3f4b974f415
Genaro Camele <genarocamele@hotmail.com>
2022-04-18 19:05:22 -0300

    Added fields to display, filter and search in UserFiles admin panel

--- src/user_files/admin.py
+++ src/user_files/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import UserFile
 
-admin.site.register(UserFile)
+
+class UserFileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'file_type', 'upload_date', 'contains_nan_values', 'number_of_rows',
+                    'number_of_samples', 'decimal_separator', 'is_public', 'is_cpg_site_id', 'platform')
+    list_filter = ('file_type', 'upload_date', 'is_public', 'is_cpg_site_id')
+    search_fields = ('name', 'description', 'user__username')
+
+
+admin.site.register(UserFile, UserFileAdmin)
