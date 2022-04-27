7b22a3a38c701de4ac315b1f612d6386483ba30e
agustinmarraco <marraco.agustin@gmail>
2022-04-26 22:26:20 -0300

    institutions changes to improve layout and styles

--- src/frontend/static/frontend/src/components/institutions/InstitutionsList.tsx
+++ src/frontend/static/frontend/src/components/institutions/InstitutionsList.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { List, Icon } from 'semantic-ui-react'
 import { DjangoInstitution } from '../../utils/django_interfaces'
+import { Nullable } from '../../utils/interfaces'
+import '../../css/institutions.css'
 
 /**
  * Component's props
@@ -10,6 +12,8 @@ interface InstitutionsListProps {
     institutions: DjangoInstitution[],
     /** Callback of 'Show users' button */
     showUsers: (institution: DjangoInstitution) => void
+    /** Selected institution to highlight */
+    selectedInstitution: Nullable<DjangoInstitution>
 }
 
 /**
@@ -21,20 +25,20 @@ export const InstitutionsList = (props: InstitutionsListProps) => {
     return (
         <List className="margin-top-5" divided relaxed verticalAlign='middle'>
             {props.institutions.map((institution) => (
-                <List.Item key={institution.id} >
+                <List.Item key={institution.id} className={props.selectedInstitution !== null && institution.id === props.selectedInstitution.id ? 'selected listItem' : 'listItem'}>
                     <List.Content floated='right'>
                         {/* Edit button */}
                         <Icon
                             name='users'
                             color='blue'
-                            className='clickable'
+                            className='clickable institutionsShowUsersIcon'
                             title='Show users '
                             onClick={() => props.showUsers(institution)}
                         />
                     </List.Content>
                     <List.Content>
-                        <List.Header>{institution.name}</List.Header>
-                        <List.Description>
+                        <List.Header className='institutionsHeader'>{institution.name}</List.Header>
+                        <List.Description className='institutionsDescription'>
                             {institution.location}
 
                             {institution.email} {institution.telephone_number}
