b5f217a2aa8b4ba4066f6fdfb49c6ac2054b71b5
Juan <juannicolash01@gmail.com>
2023-01-21 09:56:36 -0300

    generacion manual, estado de la data

--- src/frontend/static/frontend/src/components/biomarkers/types.ts
+++ src/frontend/static/frontend/src/components/biomarkers/types.ts
@@ -37,7 +37,7 @@ interface ConfirmModal {
 interface FormBiomarkerData {
     biomarkerName: string,
     biomarkerDescription: string,
-    tag: any,
+    tag: any, // se esta laburando salu2
     moleculeSelected: BiomarkerType,
     molecule: number,
     moleculesTypeOfSelection: MoleculesTypeOfSelection.INPUT | MoleculesTypeOfSelection.AREA,
