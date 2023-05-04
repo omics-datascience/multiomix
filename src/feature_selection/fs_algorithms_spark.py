4a89cb383c3e6809dc361848d6cbb0701b8abaab
Julián Muhlberger <julian.muhlberger@gmail.com>
2023-05-04 17:41:46 -0300

    Pseudocode for built requests to spark middleware

--- src/feature_selection/fs_algorithms_spark.py
+++ src/feature_selection/fs_algorithms_spark.py
@@ -22,6 +22,74 @@ def blind_search_spark(
     @return: The combination of features with the highest fitness score and the highest fitness score achieved by
     any combination of features.
     """
+
+    # Pre-requirements
+    # ****************
+    #  - An environment variable that stores the microservice url. 
+    #    By default "multiomix-aws-emr"
+    #  - Communication with microservice endpoint. (Infrastructure side)
+    #  - Shared volumen with microservice. (Infrastructure side)
+    #  - An environment variable that stores the dataset path (shared volume).
+
+    # Dataset dump
+    # ************
+    #  - Save clinical_data dataset inside the shared volumen, the path for that
+    #    file will be passed to the microservice to lookup in the shared
+    #    volume. Important, take care that the mount path for the volumen
+    #    can be different in multiomix and the middleware, knowing this,
+    #    the path should be relative from the mount point.
+
+    
+    # AWS-EMR middleware microservice communication
+    # *********************************************
+    #   
+    #  Usage for blind_search_algorithm
+    #  ********************************
+    #    - @Protocol HTTP
+    #    - @Verb POST
+    #    - @URI /schedule
+    #    - @Headers
+    #      ~ Content-Type = application/json    
+    #    - @Body json object
+    #      ~ name: job name for emr (str,optional)
+    #      ~ algorithm: 0 (int,optional, default 0)
+    #      ~ entrypoint_arguments: array of json objects that will be pased 
+    #                              as args to the main python script in EMR.
+    #                              Structure of the obj should be { key:, value:}    
+    #                              (array of json objects, default None)
+    #                              Example:
+    #                              [
+    #                                {
+    #                                  "name": "clinical_data",
+    #                                  "value": "clinical_data file relative path"
+    #                                },
+    #                                {
+    #                                  "name": "classifier",
+    #                                  "value": "classifier value"
+    #                                },
+    #                                ...
+    #                              ]    
+    #    - @Response json object
+    #      ~ id: job id in EMR
+    #      ~ name: job name in EMR
+    #      ~ virtualClusterId: cluster id in EMR     
+
+    # Recap
+    # *****
+    #   1 - Save dataset file in shared volumen.
+    #   2 - Build entrypoint arguments array.
+    #   3 - Create the request body.
+    #     3.1 - Set algorithm equals 0.
+    #     3.2 - Assign entrypoint_arguments to array created in step 2.
+    #   4 - Create HTTP request.
+    #     4.1 - Set Verb POST
+    #     4.2 - Set Host {AWS-EMR middleware microservice URL}/schedule
+    #     4.3 - Set Headers
+    #   5 - Send request.
+    #   6 - Handle response.
+
+
+
     pass
 
 
@@ -51,4 +119,70 @@ def binary_black_hole_spark(
     @return: The combination of features with the highest fitness score and the highest fitness score achieved by
     any combination of features.
     """
+
+    # Pre-requirements
+    # ****************
+    #  - An environment variable that stores the microservice url. 
+    #    By default "multiomix-aws-emr"
+    #  - Communication with microservice endpoint. (Infrastructure side)
+    #  - Shared volumen with microservice. (Infrastructure side)
+    #  - An environment variable that stores the dataset path (shared volume).
+
+    # Dataset dump
+    # ************
+    #  - Save clinical_data dataset inside the shared volumen, the path for that
+    #    file will be passed to the microservice to lookup in the shared
+    #    volume. Important, take care that the mount path for the volumen
+    #    can be different in multiomix and the middleware, knowing this,
+    #    the path should be relative from the mount point.
+
+    
+    # AWS-EMR middleware microservice communication
+    # *********************************************
+    #   
+    #  Usage for blind_search_algorithm
+    #  ********************************
+    #    - @Protocol HTTP
+    #    - @Verb POST
+    #    - @URI /schedule
+    #    - @Headers
+    #      ~ Content-Type = application/json    
+    #    - @Body json object
+    #      ~ name: job name for emr (str,optional)
+    #      ~ algorithm: 1 (int,optional, default 0)
+    #      ~ entrypoint_arguments: array of json objects that will be pased 
+    #                              as args to the main python script in EMR.
+    #                              Structure of the obj should be { key:, value:}    
+    #                              (array of json objects, default None)
+    #                              Example:
+    #                              [
+    #                                {
+    #                                  "name": "clinical_data",
+    #                                  "value": "clinical_data file relative path"
+    #                                },
+    #                                {
+    #                                  "name": "classifier",
+    #                                  "value": "classifier value"
+    #                                },
+    #                                ...
+    #                              ]
+    #    - @Response json object
+    #      ~ id: job id in EMR
+    #      ~ name: job name in EMR
+    #      ~ virtualClusterId: cluster id in EMR     
+
+    # Recap
+    # *****
+    #   1 - Save dataset file in shared volumen.
+    #   2 - Build entrypoint arguments array.
+    #   3 - Create the request body.
+    #     3.1 - Set algorithm equals 0.
+    #     3.2 - Assign entrypoint_arguments to array created in step 2.
+    #   4 - Create HTTP request.
+    #     4.1 - Set Verb POST
+    #     4.2 - Set Host {AWS-EMR middleware microservice URL}/schedule
+    #     4.3 - Set Headers
+    #   5 - Send request.
+    #   6 - Handle response.
+    
     pass
