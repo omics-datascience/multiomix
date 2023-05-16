44caaecaac055710316189e800dcb6948e417c47
Genaro Camele <genarocamele@hotmail.com>
2023-05-16 13:52:46 -0300

    Changed HeatMap colors

--- src/frontend/static/frontend/src/components/biomarkers/biomarker-details-modal/stat-validations/heatmap/constants.ts
+++ src/frontend/static/frontend/src/components/biomarkers/biomarker-details-modal/stat-validations/heatmap/constants.ts
@@ -4,15 +4,15 @@ const COLOR_LEGEND_HEIGHT = 60
 
 /** List of colors to assign to the thresholds below. */
 const COLORS = [
+    '#70e000',
+    '#dddf00',
+    '#ffcb47',
     '#ffba08',
-    '#faa307',
     '#f48c06',
     '#e85d04',
     '#dc2f02',
-    '#d00000',
     '#9d0208',
     '#6a040f',
-    '#370617',
     '#03071e'
 ]
 
