c57d97e952cf7d8441e4564b24453759fd6fad1e
Genaro Camele <genarocamele@hotmail.com>
2023-06-08 12:25:08 -0300

    Fixed URL

--- src/feature_selection/urls.py
+++ src/feature_selection/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('submit-experiment', views.FeatureSelectionSubmit.as_view(), name='feature_selection_submit'),
-    path('aws-notification/<job_id:str>/', views.FeatureSelectionExperimentAWSNotification.as_view())
+    path('aws-notification/<str:job_id>/', views.FeatureSelectionExperimentAWSNotification.as_view())
 ]
