3bd42ccb8f5a7135d844959d386dc4702538615b
Genaro Camele <genarocamele@hotmail.com>
2023-07-22 12:57:18 -0300

    Remove unused import

--- src/statistical_properties/survival_functions.py
+++ src/statistical_properties/survival_functions.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from lifelines import KaplanMeierFitter, CoxPHFitter
 from lifelines.statistics import logrank_test
-from common.datasets_utils import clinical_df_to_struct_array
 from common.utils import get_subset_of_features
 from feature_selection.fs_models import ClusteringModels
 
