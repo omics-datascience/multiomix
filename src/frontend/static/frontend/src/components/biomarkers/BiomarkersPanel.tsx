bc6b6c64ad10372c6403940a1f90fe52dee4615f
Genaro Camele <genarocamele@hotmail.com>
2024-04-13 15:26:06 -0300

    Improved all tables filters and some widths

--- src/frontend/static/frontend/src/components/biomarkers/BiomarkersPanel.tsx
+++ src/frontend/static/frontend/src/components/biomarkers/BiomarkersPanel.tsx
@@ -1554,7 +1554,7 @@ export class BiomarkersPanel extends React.Component<{}, BiomarkersPanelState> {
     }
 
     /**
-     * Generates default table's Filters
+     * Generates default table's Filters.
      * @returns Default object for table's Filters
      */
     getDefaultFilters (): PaginationCustomFilter[] {
@@ -1565,8 +1565,9 @@ export class BiomarkersPanel extends React.Component<{}, BiomarkersPanelState> {
 
         tagOptions.unshift({ key: 'no_tag', text: 'No tag' })
 
+        // TODO: refactor Tag key as it's the same as AllExperimentsView.tsx and UserFilesView.tsx
         return [
-            { label: 'Tag', keyForServer: 'tag', defaultValue: '', options: tagOptions }
+            { label: 'Tag', keyForServer: 'tag', defaultValue: '', placeholder: 'Select an existing Tag', options: tagOptions, width: 3 }
         ]
     }
 
