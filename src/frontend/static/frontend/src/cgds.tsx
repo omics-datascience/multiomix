177d90122cf4f401a9d0239c020adad862d09ed3
Juan Nicolas Herrera Delvalle <juannicolash01@gmail.com>
2024-06-18 22:52:07 -0300

    hotfix, cgds classname render

--- src/frontend/static/frontend/src/cgds.tsx
+++ src/frontend/static/frontend/src/cgds.tsx
@@ -4,6 +4,6 @@ import { createRoot } from 'react-dom/client'
 import './css/files.css'
 
 import { CGDSPanel } from './components/cgds-panel/CGDSPanel'
-const container = document.getElementById('about-us-app')
+const container = document.getElementById('cgds-app')
 const root = createRoot(container!)
 root.render(<CGDSPanel />)
