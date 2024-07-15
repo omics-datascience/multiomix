6ac9aaaa75fa5743d51da21eb1a4c04e2b5e649a
Genaro Camele <genarocamele@hotmail.com>
2024-07-15 14:28:53 -0300

    Improved export script

--- tools/export_media.sh
+++ tools/export_media.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script exports media volume (multiomics_intermediate_media_data) to a .tar.gz file in the media_backups folder.
-docker container run --rm -it -v multiomics_intermediate_media_data:/from -v ./media_backups:/to alpine ash -c "tar czfv /to/\"media-backup_$(date '+%Y-%m-%d_%H-%M-%S').tar.gz\" /from"
+docker container run --rm -it -v multiomics_intermediate_media_data:/from -v ./media_backups:/to alpine ash -c "cd /from; tar czvf /to/\"media-backup_$(date '+%Y-%m-%d_%H-%M-%S').tar.gz\" ."
