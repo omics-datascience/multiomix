d78116d45e42ea376d13d83b13910f56004f075c
Genaro Camele <genarocamele@hotmail.com>
2024-08-21 10:11:36 -0300

    Added GeneGEMCombinationAdmin

--- src/api_service/admin.py
+++ src/api_service/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Experiment, ExperimentSource, ExperimentClinicalSource
+from .models import Experiment, ExperimentSource, ExperimentClinicalSource, GeneMiRNACombination, GeneCNACombination, \
+    GeneMethylationCombination
 
 
 class ExperimentAdmin(admin.ModelAdmin):
@@ -8,6 +9,16 @@ class ExperimentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description', 'user__username')
 
 
+class GeneGEMCombinationAdmin(admin.ModelAdmin):
+    """Admin view for all GeneMiRNACombination, GeneCNACombination and GeneMethylationCombination models"""
+    list_display = ('gene', 'gem', 'correlation', 'p_value', 'adjusted_p_value', 'experiment')
+    search_fields = ('gene__name', 'gem__name', 'experiment__name')
+    ordering = ('-experiment__submit_date', 'gene', 'gem')
+
+
 admin.site.register(Experiment, ExperimentAdmin)
 admin.site.register(ExperimentSource)
 admin.site.register(ExperimentClinicalSource)
+admin.site.register(GeneMiRNACombination, GeneGEMCombinationAdmin)
+admin.site.register(GeneCNACombination, GeneGEMCombinationAdmin)
+admin.site.register(GeneMethylationCombination, GeneGEMCombinationAdmin)
