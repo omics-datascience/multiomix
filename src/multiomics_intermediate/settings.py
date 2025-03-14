1ae52a1384d9b5edbf9b376ace06220b0a84c51d
Genaro Camele <genarocamele@hotmail.com>
2025-03-14 09:32:23 -0300

    Bumped version to 5.5.0

--- src/multiomics_intermediate/settings.py
+++ src/multiomics_intermediate/settings.py
@@ -189,7 +189,7 @@ SECURE_REFERRER_POLICY = 'same-origin'
 # +++++ Custom settings +++++
 
 # Current Multiomix version
-VERSION: str = '5.4.0'
+VERSION: str = '5.5.0'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
