69c39b86e7b890d53750c8007967e6742a043864
Genaro Camele <genarocamele@hotmail.com>
2026-06-17 20:42:12 -0300

    Fixed Login page

--- src/frontend/static/frontend/src/login.tsx
+++ src/frontend/static/frontend/src/login.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { IntlProvider } from 'react-intl'
 import './css/login.css'
 
 import LoginSignUpForm from './components/login/LoginSignUpForm'
+import en from './locales/en'
+import es from './locales/es'
+
+const messages = { en, es }
 
 const container = document.getElementById('login-form')
 const root = createRoot(container!)
-root.render(<LoginSignUpForm />)
+root.render(
+    <IntlProvider locale='es' messages={messages.es}>
+        <LoginSignUpForm />
+    </IntlProvider>
+)
