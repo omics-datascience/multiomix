4af72dd30d8113716b455366948b49f934b01bbb
Genaro Camele <genarocamele@hotmail.com>
2026-06-18 11:14:17 -0300

    Fixed GEM page

--- src/frontend/static/frontend/src/gem.tsx
+++ src/frontend/static/frontend/src/gem.tsx
@@ -4,7 +4,16 @@ import './css/biomarkers-form.css'
 import './css/gem.css'
 
 import { Pipeline } from './components/pipeline/Pipeline'
+import { IntlProvider } from 'react-intl'
+import en from './locales/en'
+import es from './locales/es'
+
+const messages = { en, es }
 
 const container = document.getElementById('gem-app')
 const root = createRoot(container!)
-root.render(<Pipeline />)
+root.render(
+    <IntlProvider locale='en' messages={messages['en']}>
+        <Pipeline />
+    </IntlProvider>
+)
