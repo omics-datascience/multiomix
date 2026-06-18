8d8d819667c0dcc3a03f83fb1159cf77a92235d9
Genaro Camele <genarocamele@hotmail.com>
2026-06-18 11:14:46 -0300

    Fixed Biomarkers page

--- src/frontend/static/frontend/src/biomarkers.tsx
+++ src/frontend/static/frontend/src/biomarkers.tsx
@@ -5,7 +5,16 @@ import './css/biomarkers-form.css'
 import './css/biomarkers.css'
 
 import { BiomarkersPanel } from './components/biomarkers/BiomarkersPanel'
+import { IntlProvider } from 'react-intl'
+import en from './locales/en'
+import es from './locales/es'
+
+const messages = { en, es }
 
 const container = document.getElementById('biomarkers-app')
 const root = createRoot(container!)
-root.render(<BiomarkersPanel />)
+root.render(
+    <IntlProvider locale='en' messages={messages['en']}>
+        <BiomarkersPanel />
+    </IntlProvider>
+)
